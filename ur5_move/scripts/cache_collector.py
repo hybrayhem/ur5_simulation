7936531d9ffc55ca78abaea72275f789920fd8cb
Halil <halil000ibrahim@gmail.com>
2021-01-26 22:59:52 +0300

    rename

---
+++ ur5_move/scripts/cache_collector.py
@@ -0,0 +1,234 @@
+# Python 2/3 compatibility imports
+from __future__ import print_function
+from six.moves import input
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+## END_SUB_TUTORIAL
+
+
+def all_close(goal, actual, tolerance):
+  """
+  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
+  @param: goal       A list of floats, a Pose or a PoseStamped
+  @param: actual     A list of floats, a Pose or a PoseStamped
+  @param: tolerance  A float
+  @returns: bool
+  """
+  all_equal = True
+  if type(goal) is list:
+    for index in range(len(goal)):
+      if abs(actual[index] - goal[index]) > tolerance:
+        return False
+
+  elif type(goal) is geometry_msgs.msg.PoseStamped:
+    return all_close(goal.pose, actual.pose, tolerance)
+
+  elif type(goal) is geometry_msgs.msg.Pose:
+    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+
+  return True
+
+
+class CacheCollector(object):
+  
+  def __init__(self):
+    super(CacheCollector, self).__init__()
+
+    ## BEGIN_SUB_TUTORIAL setup
+    ##
+    ## First initialize `moveit_commander`_ and a `rospy`_ node:
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('cache_collector', anonymous=True)
+
+    ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+    ## kinematic model and the robot's current joint states
+    robot = moveit_commander.RobotCommander()
+
+    ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+    ## for getting, setting, and updating the robot's internal understanding of the
+    ## surrounding world:
+    scene = moveit_commander.PlanningSceneInterface()
+
+    ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+    ## to a planning group (group of joints).  In this tutorial the group is the primary
+    ## arm joints in the Panda robot, so we set the group's name to "panda_arm".
+    ## If you are using a different robot, change this value to the name of your robot
+    ## arm planning group.
+    ## This interface can be used to plan and execute motions:
+    group_name = "manipulator"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+
+    ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
+    ## trajectories in Rviz:
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                   moveit_msgs.msg.DisplayTrajectory,
+                                                   queue_size=20)
+
+    ## END_SUB_TUTORIAL
+
+    ## BEGIN_SUB_TUTORIAL basic_info
+    ##
+    ## Getting Basic Information
+    ## ^^^^^^^^^^^^^^^^^^^^^^^^^
+    # We can get the name of the reference frame for this robot:
+    planning_frame = move_group.get_planning_frame()
+    print("============ Planning frame: %s" % planning_frame)
+
+    # We can also print the name of the end-effector link for this group:
+    eef_link = move_group.get_end_effector_link()
+    print("============ End effector link: %s" % eef_link)
+
+    # We can get a list of all the groups in the robot:
+    group_names = robot.get_group_names()
+    print("============ Available Planning Groups:", robot.get_group_names())
+
+    # Sometimes for debugging it is useful to print the entire state of the
+    # robot:
+    print("============ Printing robot state")
+    print(robot.get_current_state())
+    print("")
+    ## END_SUB_TUTORIAL
+
+    # Misc variables
+    self.box_name = ''
+    self.robot = robot
+    self.scene = scene
+    self.move_group = move_group
+    self.display_trajectory_publisher = display_trajectory_publisher
+    self.planning_frame = planning_frame
+    self.eef_link = eef_link
+    self.group_names = group_names
+
+
+  def go_to_joint_state(self):
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+    move_group = self.move_group
+
+    ## BEGIN_SUB_TUTORIAL plan_to_joint_state
+    ##
+    ## Planning to a Joint Goal
+    ## ^^^^^^^^^^^^^^^^^^^^^^^^
+    ## The Panda's zero configuration is at a `singularity <https://www.quora.com/Robotics-What-is-meant-by-kinematic-singularity>`_ so the first
+    ## thing we want to do is move it to a slightly better configuration.
+    # We can get the joint values from the group and adjust some of the values:
+    joint_goal = move_group.get_current_joint_values()
+    joint_goal[0] = 0
+    joint_goal[1] = -pi/4
+    joint_goal[2] = 0
+    joint_goal[3] = -pi/2
+    joint_goal[4] = 0
+    joint_goal[5] = pi/3
+    joint_goal[6] = 0
+
+    # The go command can be called with joint values, poses, or without any
+    # parameters if you have already set the pose or joint target for the group
+    move_group.go(joint_goal, wait=True)
+
+    # Calling ``stop()`` ensures that there is no residual movement
+    move_group.stop()
+
+    ## END_SUB_TUTORIAL
+
+    # For testing:
+    current_joints = move_group.get_current_joint_values()
+    return all_close(joint_goal, current_joints, 0.01)
+
+
+  def go_to_pose_goal(self, x, y, z, w):
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+    move_group = self.move_group
+
+    ## BEGIN_SUB_TUTORIAL plan_to_pose
+    ##
+    ## Planning to a Pose Goal
+    ## ^^^^^^^^^^^^^^^^^^^^^^^
+    ## We can plan a motion for this group to a desired pose for the
+    ## end-effector:
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.w = w #1.0
+    pose_goal.position.x = x #0.4
+    pose_goal.position.y = y #0.1
+    pose_goal.position.z = z #0.4
+
+    move_group.set_pose_target(pose_goal)
+
+    ## Now, we call the planner to compute the plan and execute it.
+    plan = move_group.go(wait=True)
+    # Calling `stop()` ensures that there is no residual movement
+    move_group.stop()
+    # It is always good to clear your targets after planning with poses.
+    # Note: there is no equivalent function for clear_joint_value_targets()
+    move_group.clear_pose_targets()
+
+    ## END_SUB_TUTORIAL
+
+    # For testing:
+    # Note that since this section of code will not be included in the tutorials
+    # we use the class variable rather than the copied state variable
+    current_pose = self.move_group.get_current_pose().pose
+    return all_close(pose_goal, current_pose, 0.01)
+
+def main():
+  try:
+
+    cache_collector = CacheCollector()
+    while(True):
+      x, y, z, w = input("Enter three x,y,z and w value: ").split()
+      cache_collector.go_to_pose_goal(float(x), float(y), float(z), float(w))
+
+  except rospy.ROSInterruptException:
+    return
+  except KeyboardInterrupt:
+    return
+
+if __name__ == '__main__':
+  main()
+
+## BEGIN_TUTORIAL
+## .. _moveit_commander:
+##    http://docs.ros.org/noetic/api/moveit_commander/html/namespacemoveit__commander.html
+##
+## .. _MoveGroupCommander:
+##    http://docs.ros.org/noetic/api/moveit_commander/html/classmoveit__commander_1_1move__group_1_1MoveGroupCommander.html
+##
+## .. _RobotCommander:
+##    http://docs.ros.org/noetic/api/moveit_commander/html/classmoveit__commander_1_1robot_1_1RobotCommander.html
+##
+## .. _PlanningSceneInterface:
+##    http://docs.ros.org/noetic/api/moveit_commander/html/classmoveit__commander_1_1planning__scene__interface_1_1PlanningSceneInterface.html
+##
+## .. _DisplayTrajectory:
+##    http://docs.ros.org/noetic/api/moveit_msgs/html/msg/DisplayTrajectory.html
+##
+## .. _RobotTrajectory:
+##    http://docs.ros.org/noetic/api/moveit_msgs/html/msg/RobotTrajectory.html
+##
+## .. _rospy:
+##    http://docs.ros.org/noetic/api/rospy/html/
+## CALL_SUB_TUTORIAL imports
+## CALL_SUB_TUTORIAL setup
+## CALL_SUB_TUTORIAL basic_info
+## CALL_SUB_TUTORIAL plan_to_joint_state
+## CALL_SUB_TUTORIAL plan_to_pose
+## CALL_SUB_TUTORIAL plan_cartesian_path
+## CALL_SUB_TUTORIAL display_trajectory
+## CALL_SUB_TUTORIAL execute_plan
+## CALL_SUB_TUTORIAL add_box
+## CALL_SUB_TUTORIAL wait_for_scene_update
+## CALL_SUB_TUTORIAL attach_object
+## CALL_SUB_TUTORIAL detach_object
+## CALL_SUB_TUTORIAL remove_object
+## END_TUTORIAL
+
